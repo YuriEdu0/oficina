90698ad113399e1a4b27bdea0317286424e93cb8
yuri <yurieduardo2704@gmail.com>
2025-11-07 21:32:33 -0400

    commit inicial

---
+++ src/main/java/com/mycompany/oficina/Oficina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oficina;
+
+/**
+ *
+ * @author oem
+ */
+public class Oficina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
